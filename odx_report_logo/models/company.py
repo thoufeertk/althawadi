7359a8471c3b8d67512ba661141e1e51d6f0af8f
Fathimma-Murad <fam@odoxsofthub.com>
2023-02-16 16:51:22 +0530

    Added Updated Workshop Quotation

--- odx_report_logo/models/company.py
+++ odx_report_logo/models/company.py
@@ -1,8 +1,10 @@
-from odoo import fields,models,api
+from odoo import fields, models, api
+
 
 class res_company(models.Model):
 
     _inherit = "res.company"
-    header_image =fields.Image(string="Header",  max_width=1920, max_height=1920,force_save=True)
-    footer_image =fields.Image(string="Footer", force_save=True)
+    header_image = fields.Image(string="Header",  max_width=1920, max_height=1920,force_save=True)
+    footer_image = fields.Image(string="Footer", force_save=True)
+    stamp_image = fields.Image(string="Stamp", force_save=True)
 
